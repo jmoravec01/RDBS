f1cf7a2d5a4ecc0e230fac4f01b912cf0bd74f2b
jmoravec01 <kubik.moravec@email.cz>
2023-02-08 13:56:34 +0100

    update

--- static/alert_anime.js
+++
@@ -1,7 +0,0 @@
-if (document.querySelector('.js-alert')) {
-  document.querySelectorAll('.js-alert').forEach(function($el) {
-    setTimeout(() => {
-      $el.classList.remove('show');
-    }, 2000);
-  });
-}
--- static/refresh_button.js
+++
@@ -1,8 +0,0 @@
-function refreshContent() {
-    let refreshIcon = document.getElementById("refresh-icon")
-    let refreshButton = document.getElementById("refresh-button")
-    refreshIcon.style.animationPlayState = "running"
-    refreshButton.disabled = true
-    }
-    
-    
